e6a0da7d8d10d1d4ab5cd50d7f17ad64cd565828
adamgaafar <dodogaafar.ag@gmail.com>
2022-12-17 14:18:04 +0200

    Initial commit

---
+++ Topics/Standard output/We need to learn Kotlin/src/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    /*
+  WE NEED
+
+  TO LEARN KOTLIN
+
+  AS QUICKLY AS POSSIBLE
+    */
+    println("WE NEED")
+    println()
+    println("TO LEARN KOTLIN")
+    println()
+    println("AS QUICKLY AS POSSIBLE")    
+}
